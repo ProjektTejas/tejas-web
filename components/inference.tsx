d38969cc4a61f034f16f85c516d1c82457becd71
Satyajit Ghana <satyajitghana7@gmail.com>
2021-01-06 23:30:23 +0530

    update

--- components/inference.tsx
+++ components/inference.tsx
@@ -1,9 +1,23 @@
+import Title from "antd/lib/typography/Title";
+
 interface Props {
     showModal: any;
 }
 
 const Inference = (props: Props) => {
-    return <div>Inference</div>;
+    return (
+        <div>
+            <Title
+                style={{
+                    textAlign: "center",
+                    fontFamily: "IBM Plex Mono",
+                    fontSize: "4em",
+                }}
+            >
+                Inference
+            </Title>
+        </div>
+    );
 };
 
 export default Inference;
