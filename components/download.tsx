968aa27c7767775e6c54c016ae03098caebf6b03
Satyajit Ghana <satyajitghana7@gmail.com>
2021-01-07 23:31:01 +0530

    update

--- components/download.tsx
+++ components/download.tsx
@@ -1,9 +1,23 @@
+import Title from "antd/lib/typography/Title";
+
 interface Props {
     showModal: any;
 }
 
 const Download = (props: Props) => {
-    return <div>Download</div>;
+    return (
+        <div>
+            <Title
+                style={{
+                    textAlign: "center",
+                    fontFamily: "IBM Plex Mono",
+                    fontSize: "4em",
+                }}
+            >
+                Download
+            </Title>
+        </div>
+    );
 };
 
 export default Download;
